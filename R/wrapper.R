20e591b66088ccb60a6a71f24e1998767948eee5
Jacqueline Mauro <jacqueline.amauro@gmail.com>
2017-05-03 12:19:50 -0400

    Oops, was predicting on training set. Got that worked out. Now estimates look like they make more sense. Should be ready to go and user friendlyish. Not fully tested but hey.

--- R/wrapper.R
+++ R/wrapper.R
@@ -8,23 +8,23 @@
 #'
 #' @return an estimate of the causal effect
 
-CACE<-function(y,a,z,cov){
+CACE<-function(y,a,z,cov,delta=2){
 
   # split data
-  data <- as.data.frame(cbind(y,a,z,cov))
+  data    <- as.data.frame(cbind(y,a,z,cov))
   mysplit(data)
 
   # estimates on first split
   means1  = mean_est(y=ds1[,1],a = ds1[,2],z = ds1[,3],cov = ds1[,c(4:dim(ds1)[2])])
   prop1   = propscore_est(y=ds1[,1],x=ds1[,c(4:dim(cov)[2])])
-  phi1    = phi_est(y=ds1[,1],a = ds1[,2],z = ds1[,3],cov = ds1[,c(4:dim(ds2)[2])]
-                    ,ymean=ymean,amean=amean,p=prop1)
+  phi1    = phi_est(y=ds2[,1],a = ds2[,2],z = ds2[,3],cov = ds2[,c(4:dim(ds2)[2])]
+                    ,ymean=ymean,amean=amean,p=prop1,delta=delta)
 
   # estimates on second split
   means2  = mean_est(y=ds2[,1],a = ds2[,2],z = ds2[,3],cov = ds2[,c(4:dim(ds2)[2])])
   prop2   = propscore_est(y=ds2[,1],x=ds2[,c(4:dim(cov)[2])])
-  phi2    = phi_est(y=ds2[,1],a = ds2[,2],z = ds2[,3],cov = ds2[,c(4:dim(ds2)[2])]
-                    ,ymean=ymean,amean=amean,p=prop2)
+  phi2    = phi_est(y=ds1[,1],a = ds1[,2],z = ds1[,3],cov = ds1[,c(4:dim(ds1)[2])]
+                    ,ymean=ymean,amean=amean,p=prop2,delta=delta)
 
   # average
   phi     = mean(.5*(phi1 + phi2),na.rm = T)
