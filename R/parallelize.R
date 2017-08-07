81a2194c8007e83a74046349f5bb40075f9492a2
Jacqueline Mauro <jacqueline.amauro@gmail.com>
2017-08-07 16:16:14 -0400

    really just trying to get the parrallel code to figure out i updated the mean estimation code

--- R/parallelize.R
+++ R/parallelize.R
@@ -1,32 +1,36 @@
 # this is not doing what it needs to
 # it can't seem to find packages that have been loaded elsewhere
 
-# library(parallel)
-# no_cores <- detectCores() - 1
-# cl <- makeCluster(no_cores)
-#
-# dummyTest<-function(dat){
-#   require(CACE)
-#   CACE:::mean_est(y=dat[,1],a = dat[,2],z = dat[,3],cov = dat[,c(4:dim(dat)[2])])
-# }
-#
-# getMeanProp<-function(dat,ranger = ranger){
-#   if(ranger == F){
-#     # estimate means using what you've specified
-#     means  = mean_est(y=dat[,1],a = dat[,2],z = dat[,3],cov = dat[,c(4:dim(dat)[2])])
-#   }
-#   else{
-#     # estimates neans using ranger
-#     means  = mean_est_ranger(y=dat[,1],a = dat[,2],z = dat[,3],cov = dat[,c(4:dim(dat)[2])])
-#   }
-#
-#   # get propensity score
-#   prop   = propscore_est(y=dat[,3],x=dat[,c(4:dim(dat)[2])])
-#
-#   return(list(means,prop))
-# }
-#
-# test = parLapply(cl,list(ds1,ds2), dummyTest)
-#
-#
-# stopCluster(cl)
+library(parallel)
+no_cores <- detectCores() - 1
+cl <- makeCluster(no_cores)
+
+dummyTest<-function(dat){
+  require(CACE); require('ranger')
+  #CACE:::mean_est_ranger(y=dat[,1],a = dat[,2],z = dat[,3],cov = dat[,c(4:dim(dat)[2])])
+  CACE:::propscore_est(y=dat[,3],x=dat[,c(4:dim(dat)[2])])
+  }
+
+getMeanProp<-function(dat,ranger = T){
+  require(CACE); require('ranger')
+  if(ranger == F){
+    # estimate means using what you've specified
+    means  = CACE:::mean_est(y=dat[,1],a = dat[,2],z = dat[,3],cov = dat[,c(4:dim(dat)[2])])
+  }
+  else{
+    # estimates neans using ranger
+    means  = CACE:::mean_est_ranger(y=dat[,1],a = dat[,2],z = dat[,3],cov = dat[,c(4:dim(dat)[2])])
+  }
+
+  # get propensity score
+  prop   = CACE:::propscore_est(y=dat[,3],x=dat[,c(4:dim(dat)[2])])
+
+  return(list(means,prop))
+}
+
+start = proc.time()
+test = parLapply(cl,list(ds1,ds2), getMeanProp)
+end = proc.time()
+
+
+stopCluster(cl)
